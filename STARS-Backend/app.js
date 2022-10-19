c6b50a2a372440eb73286f28fd9d58e9421d8b86
David <dabarcenas6921@gmail.com>
2022-10-18 20:33:51 -0400

    added auth route

--- STARS-Backend/app.js
+++ STARS-Backend/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const authRoute = require("./routes/auth.js");
 
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
+
+//ROUTES
+app.use("/auth", authRoute);
 
 // health check
 app.get("/", function (req, res) {
