05f95e883da72bb966e6a183866d42c1b67e6f8b
David <dabarcenas6921@gmail.com>
2022-10-28 00:54:51 -0400

    created new tables: courses, schedules, appointments

--- STARS-Backend/star-schema.sql
+++ STARS-Backend/star-schema.sql
@@ -6,5 +6,26 @@ CREATE TABLE users(
     last_name       TEXT NOT NULL,
     email           TEXT NOT NULL UNIQUE CHECK (POSITION('@' IN email) > 1),
     created_at      TIMESTAMP NOT NULL DEFAULT NOW(),
-    panther_id      INTEGER NOT NULL
+    panther_id      INTEGER
+);
+
+CREATE TABLE courses (
+    course         TEXT PRIMARY KEY,
+    tutors         INTEGER[]
+);
+
+CREATE TABLE schedules (
+    tutor_id           INTEGER PRIMARY KEY,
+    tutor_schedules    TIMESTAMP[][] NOT NULL,
+    FOREIGN KEY (tutor_id) REFERENCES users(id)  
+);
+
+CREATE TABLE appointments(
+    appointment_id     SERIAL PRIMARY KEY,
+    student_id         INTEGER NOT NULL,
+    tutor_id           INTEGER NOT NULL,
+    app_start_time     TIMESTAMP NOT NULL,
+    app_end_time       TIMESTAMP NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES users(id),
+    FOREIGN KEY (tutor_id) REFERENCES users(id)
 );
